75eb2764fed9851b07590dd2ca85fe7aeaf6348a
Md Sujon Islam <iamsujonislam06@gmail.com>
2026-03-10 23:03:16 +0600

    project link update

--- src/ui/Project.jsx
+++ src/ui/Project.jsx
@@ -80,15 +80,15 @@ const Project = () => {
             <ul className="text-xs md:text-sm tracking-wide flex gap-2 md:gap-5 justify-between text-darkColor">
               <li>ReactJs</li>
               <li>JavaScript</li>
-              <li>Next-auth</li>
-              <li>Stripe</li>
-              <li>Vercel Deployment</li>
+              <li>NodeJs</li>
+              <li>MongoDB</li>
+              
             </ul>
           </div>
         </div>
         <div className="flex flex-col lg:flex-row gap-6 py-8 my-16">
           <a
-            href="https://shslira.com/"
+            href="https://heavenliramarketingltd.com/"
             className="w-full lg:w-1/2 h-auto relative group  rounded-lg overflow-hidden"
           >
             <img
@@ -101,26 +101,26 @@ const Project = () => {
 
           <div className="w-full lg:w-1/2 flex flex-col gap-6 lg:justify-between items-end text-right lg:-ml-12 z-10">
             <div>
-              <h3 className="text-2xl font-bold">SHS Enterprise Ltd </h3>
+              <h3 className="text-2xl font-bold">Heaven Lira Marketing Ltd </h3>
             </div>
             <div>
               <p className="text-designColor text-sm tracking-wide">
                 Build Project
               </p>
-              <h3 className="text-2xl font-bold">SHS Enterprise Ltd 2.0</h3>
+              <h3 className="text-2xl font-bold">Heaven Lira Marketing Ltd 2.0</h3>
             </div>
             <p className="bg-[#112240] text-sm md:text-base p-2 md:p-6 rounded-md lg:-ml-16">
-              SHS Enterprise Ltd clone website for visualizing personalized SHS
-              Enterprise Ltd website. View your products, Add your account with{" "}
+              Heaven Lira Marketing Ltd clone website for visualizing personalized Heaven Lira Marketing Ltd
+              website. View your products, Add your account with{" "}
               <span className="text-green-500">O-auth</span> and then make the
               purchage using <span className="text-designColor">stripe</span>
             </p>
             <ul className="text-xs md:text-sm tracking-wide flex gap-2 md:gap-5 justify-between text-darkColor">
               <li>ReactJs</li>
               <li>JavaScript</li>
-              <li>Next-auth</li>
-              <li>Stripe</li>
-              <li>Vercel Deployment</li>
+              <li>NodeJs</li>
+              <li>MongoDB</li>
+             
             </ul>
           </div>
         </div>
